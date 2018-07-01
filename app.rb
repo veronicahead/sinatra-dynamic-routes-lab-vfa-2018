eee08daaad8c3274c58acc67cfa8a270f3c09cb2
Veronica Head <veronica.leigh.head@gmail.com>
2018-07-01 01:52:46 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -30,6 +30,9 @@ class App < Sinatra::Base
   end
   
   get '/:operation/:number1/:number2' do
-    
+    @operation = params[:operation]
+    @number1 = params[:number1]
+    @number2 = params[:number2]
+    if o
   
 end
